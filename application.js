bd78599f2a78db4cbb95c00b8d4372e4ef4b4c60
Hilverd Reker <hilverd@hilverdreker.com>
2023-10-22 19:36:33 +0100

    Retrieve version from application.js

--- application.js
+++ application.js
@@ -63,6 +63,7 @@ document.addEventListener('alpine:init', () => {
     const paths = forestPaths(forest);
 
     Alpine.data('bookmarks', () => ({
+        version: '0.0.1',
         xHtmlForForest: forest.map((tree) => `<ul>${xHtmlForTree(tree)}</ul>`),
         searchString: '',
         activeSearchResultIndex: 0,
